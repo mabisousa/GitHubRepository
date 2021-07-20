d4b03cf50076056b4c0668f0cd2dcf3e5a5f7d92
mabisousa <maria_gs_cruz@estudante.sc.senai.br>
2021-07-20 16:10:11 -0300

    Configuração para listagem de repositório git

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,86 +1,95 @@
-import React, { useState, FormEvent, useEffect } from 'react';
-import api from '../../services/api';
-import Repository from '../Repository';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import api from '../../services/api';
 
 import { Title, Repositories, Form, Error } from './style';
 
-interface Repository{
-  full_name: string;
-  description: string;
-  owner:{
-    login: string;
-    avatar_url: string;
-  }
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    }
 }
 
 const Dashboard: React.FC = () => {
-  const [newRepo, setNewRepo] = useState('');
-  const [inputError, setInputError] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>(() =>{
-    const storageRepository = localStorage.getItem(
-      '@GitHubExplorer:repositories'
-    )
-    if(storageRepository){
-      return JSON.parse(storageRepository);
+    const [newRepo, setNewRepo] = useState('');
+    const [inputError, setInputError] = useState('');
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        const storageRepository = localStorage.getItem(
+            '@GithubExplorer:repositories',
+        );
+
+        if(storageRepository){
+            return JSON.parse(storageRepository)
+        }
+
+        return [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem(
+            '@GithubExplorer:repositories',
+            JSON.stringify(repositories)
+        )
+    }, [repositories]);
+
+    async function handleAddRepository(
+        event: FormEvent<HTMLFormElement>,
+    ): Promise<void> {
+        event.preventDefault();
+
+        if(!newRepo){
+            setInputError("Digite um usuário/repositório para pesquisar.");
+            return;
+        }
+
+        try{
+            const response = await api.get<Repository>(`repos/${newRepo}`);
+            const repository = response.data;
+
+            setRepositories([...repositories, repository]);
+            setNewRepo('');
+            setInputError('');
+
+        } catch(err){
+            setInputError("Repositório não encontrado ou inexistente");
+        }
     }
-    return [];
-  });
 
-  useEffect(() => {
-    localStorage.setItem(
-      '@GitHubExplorer:repositories', JSON.stringify(repositories)
-    )
-  }, [repositories]);
+    return (
+        <>
+            <Title>Explore repositórios no GitHub</Title>
 
-  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
-    event.preventDefault();
+            <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+                <input
+                    value={newRepo}
+                    onChange={e => setNewRepo(e.target.value)}
+                    placeholder="Digite o nome do repositório" />
+                <button type="submit">Pesquisar</button>
+            </Form>
 
-    if(!newRepo){
-      setInputError("Digite um usuário/repositório para pesquisar.")
-      return;
-    }
+            {inputError && <Error>{inputError}</Error>}
 
-    try{
-      const response = await api.get<Repository>(`repos/${newRepo}`);
-      const repository = response.data;
-
-      setRepositories([...repositories, repository]);
-      setNewRepo('');
-      setInputError('');
-    }catch(err){
-      setInputError("Repositório não encontrada ou inexistente");
-    }
-
-  }
-
-  return (
-    <>
-      <Title>Explore repositórios no GitHub</Title>
-
-      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
-        <input value={newRepo} onChange={e => setNewRepo(e.target.value)} placeholder="Digite o nome do repositório" />
-        <button type="submit">Pesquisar</button>
-      </Form>
-
-      {inputError && <Error>{inputError}</Error>}
-
-      <Repositories>
-
-        {repositories.map(repository => (
-          <Link key={repository.full_name} to={`/repository/${repository.full_name}`}>
-              <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
-              <div>
-                <strong>{repository.full_name}</strong>
-                <p>{repository.description}</p>
-              </div>
-              <FiChevronRight size={20} color='#000'/>
-          </Link>
-        ))}
-      </Repositories>
-    </>
-  );
+            <Repositories>
+                {repositories.map(repository => (
+                    <Link key={repository.full_name} to={`/repository/${repository.full_name}`}>
+                        <img
+                            src={repository.owner.avatar_url}
+                            alt={repository.owner.login}
+                        />
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+                        <FiChevronRight size={20} />
+                    </Link>
+                ))}
+            </Repositories>
+        </>
+    );
 };
 
 export default Dashboard;
--- src/Pages/Repository/index.tsx
+++ src/Pages/Repository/index.tsx
@@ -1,13 +1,99 @@
-import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues } from './style';
+import api from '../../services/api';
+import logoImg from '../../assets/logo.svg';
 
 interface RepositoryParams{
   repository: string;
 }
 
+interface Repositorio{
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner:{
+    login: string;
+    avatar_url: string;
+  }
+}
+
+interface Issues{
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Repository: React.FC = () => {
+  const [repository, setRepository] = useState<Repositorio     | null>(null);
+  const [issues, setIssues] = useState<Issues[]>([]);
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>Repository : {params.repository}</h1>
+
+  useEffect(() => {
+    api.get(`repos/${params.repository}`).then((response) => {
+      setRepository(response.data);
+    });
+
+    api.get(`repos/${params.repository}/issues`).then((response) => {
+      setIssues(response.data);
+    });
+  }, [params.repository]);
+
+  return (
+    <>
+    <Header>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Link to="/">
+        <FiChevronLeft size={15}/>
+        Voltar</Link>
+    </Header>
+    {repository && (
+    <RepositoryInfo>
+      <header>
+        <img src={repository.owner.avatar_url} alt={repository.owner.login  } />
+        <div>
+          <strong>{repository.full_name}</strong>
+          <p>{repository.description}</p>
+        </div>
+      </header>
+      <ul>
+        <li>
+          <strong>{repository.stargazers_count}</strong>
+          <span>Stars</span>
+        </li>
+        <li>
+          <strong>{repository.forks_count}</strong>
+          <span>Forks</span>
+        </li>
+        <li>
+          <strong>{repository.open_issues_count}</strong>
+          <span>Issues abertas</span>
+        </li>
+      </ul>
+    </RepositoryInfo>
+    )}
+
+    <Issues>
+      {issues.map(issue => (
+        <a key={issue.id} href={issue.html_url} target="blanck">
+          <img src={issue.user.avatar_url} alt={issue.user.login} />
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
+            <FiChevronRight size={20} color='#000'/>
+        </a>
+      ))}
+      </Issues>
+    </>
+  )
 };
 
 export default Repository;
